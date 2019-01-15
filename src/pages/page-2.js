57362de9eb93798e8c7f2104a3c86dd4c9a61714
Max Ma <max@batch.nz>
2019-01-15 13:53:15 +1300

    Revert "Add a fake form again to active a roles field."
    This reverts commit c0a01eac74da5ee85dd4646ceb4fd7e2101cecd4.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -15,24 +15,7 @@ const Page2 = ({ location }) => {
         description="This is the Page 2 description"
       />
       <h1 className="text-4xl leading-tight mb-4">Page 2</h1>
-      {/* <Form /> */}
-      <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="name" placeholder="name" />
-        <input type="email" name="email" placeholder="email" />
-        <input type="textarea" name="message" placeholder="leave a message" />
-        <input type="text" name="newsletter" placeholder="newsletter" />
-        <input type="text" name="roles" placeholder="newsletter" />
-        <input type="text" name="plan" placeholder="plan" />
-        <button type="submit">Send</button>
-      </form>
-
+      <Form />
       <Link cover to="/">
         Go Home
       </Link>
