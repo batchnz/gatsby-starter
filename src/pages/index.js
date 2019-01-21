7b257f83231828e20f590610b60da98627eddc7d
Max Ma <max@batch.nz>
2019-01-21 15:35:07 +1300

    Update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,12 @@ const IndexPage = ({ location }) => {
       <h1 className="text-4xl leading-tight mb-4">Home</h1>
       <div className="rte">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          consequatur.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor eos
+          labore iusto id nesciunt in dicta quas! Officiis voluptates incidunt
+          praesentium ea, natus quod. Explicabo veritatis vel blanditiis aliquam
+          eos nihil magni asperiores, mollitia animi aspernatur saepe vero illum
+          odio praesentium? Perferendis eos sint blanditiis assumenda ut
+          obcaecati! Quisquam, quaerat.
         </p>
       </div>
     </Layout>
