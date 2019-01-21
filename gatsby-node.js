b8e149e0ec3c143c99d3c03c66b5a725ec1d4642
Alex Hatzipanis <alex@hatzipanis.com>
2019-01-21 18:09:27 +1300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
 exports.onCreateWebpackConfig = ({ actions, stage }) => {
   if (stage.includes('develop')) {
     actions.setWebpackConfig({
